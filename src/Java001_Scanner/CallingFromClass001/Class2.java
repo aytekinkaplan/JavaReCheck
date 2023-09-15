a8b39260c2f320de0e7522b98aaecfccec5c26e6
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-16 01:38:58 +0300

    New Commit

---
+++ src/Java001_Scanner/CallingFromClass001/Class2.java
@@ -0,0 +1,9 @@
+package Java001_Scanner.CallingFromClass001;
+
+public class Class2 {
+    public static void main(String[] args) {
+        Class1 class1 = new Class1();
+        class1.nameCalling(class1.name);
+    }
+
+}
