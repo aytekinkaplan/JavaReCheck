100e548b2e250fa12e3519a54016622e7dfd2eeb
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-16 17:47:19 +0300

    New Commit

---
+++ src/Java001_Scanner/CallingFromClass008/ClassY.java
@@ -0,0 +1,10 @@
+package Java001_Scanner.CallingFromClass008;
+
+import Java001_Scanner.CallingFromClass007.ClassA;
+
+public class ClassY {
+    public static void main(String[] args) {
+        ClassX classX = new ClassX();
+        classX.calculatingVKE(classX.mass, classX.height);
+    }
+}
