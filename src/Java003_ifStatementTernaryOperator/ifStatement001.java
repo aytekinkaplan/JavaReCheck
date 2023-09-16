4c9dd193a274449617fbdf4c7047e010a3e26e9e
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-17 00:34:42 +0300

    17/09/23 dated committing

---
+++ src/Java003_ifStatementTernaryOperator/ifStatement001.java
@@ -0,0 +1,13 @@
+package Java003_ifStatementTernaryOperator;
+
+public class ifStatement001 {
+    public static void main(String[] args) {
+        int number1 = 34;
+        int number2 = 35;
+        if (number1==number2){
+            System.out.println("Siz ikiniz ikizsiniz");
+        }else {
+            System.out.println("Siz ikiniz ikiz değilsiniz");
+        }
+    }
+}
