6f2e1b38ae61406b32af2443b0018139cebe3353
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-17 00:14:55 +0300

    17/09/23 dated committing

---
+++ src/Java002_Operators/ArithMeticExample004.java
@@ -0,0 +1,18 @@
+package Java002_Operators;
+
+public class ArithMeticExample004 {
+    public static void main(String[] args) {
+        int x = 8;
+        System.out.println(x);
+        x+=3;
+        System.out.println(x);
+        x-=4;
+        System.out.println(x);
+        x*=3;
+        System.out.println(x);
+        x/=2;
+        System.out.println(x);
+        x%=3;
+        System.out.println(x);
+    }
+}
