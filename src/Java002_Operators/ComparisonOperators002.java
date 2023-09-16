d9e0d71e58b17d58dbf255f2051d83fc656004e9
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-17 00:17:33 +0300

    17/09/23 dated committing

---
+++ src/Java002_Operators/ComparisonOperators002.java
@@ -0,0 +1,22 @@
+package Java002_Operators;
+
+import java.util.Scanner;
+
+public class ComparisonOperators002 {
+
+    public static void main(String[] args) {
+        float float1 =12.34f;
+        float float2 =12.34f;
+        System.out.println(float1 == float2);
+
+        float float3 =14f;
+        float float4 =17f;
+        System.out.println(float3 == float4);
+
+        double double01=12.34;
+        double double02=12.00;
+
+        System.out.println(double01!=double02);
+
+    }
+}
