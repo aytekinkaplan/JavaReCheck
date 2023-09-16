a272e9699a0343c815736791a1598064391422c9
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-17 00:25:08 +0300

    17/09/23 dated committing

---
+++ src/Java002_Operators/LogicOperators001.java
@@ -0,0 +1,12 @@
+package Java002_Operators;
+
+public class LogicOperators001 {
+    public static void main(String[] args) {
+        boolean yes = true;
+        boolean no = false;
+        int A = 23;
+        int B = 34;
+
+
+    }
+}
