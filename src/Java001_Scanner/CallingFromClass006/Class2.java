31be45f0874aa9c45bdcf2e0b1ef0b9cd13c18d0
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-16 16:21:05 +0300

    New Commit

---
+++ src/Java001_Scanner/CallingFromClass006/Class2.java
@@ -0,0 +1,8 @@
+package Java001_Scanner.CallingFromClass006;
+
+public class Class2 {
+    public static void main(String[] args) {
+        Class1 class1 = new Class1();
+        class1.hesapVer(class1.hesap);
+    }
+}
