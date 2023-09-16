379f4a5407a9a20eb0b54d698ab556618fa57b28
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-17 00:17:06 +0300

    17/09/23 dated committing

---
+++ src/Java001_Scanner/CallingFromClass016/Cls02.java
@@ -0,0 +1,8 @@
+package Java001_Scanner.CallingFromClass016;
+
+public class Cls02 {
+    public static void main(String[] args) {
+        Cls01 cls01 = new Cls01();
+        cls01.chars(cls01.input);
+    }
+}
