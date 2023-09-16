aef599ee74799768e1970446e0f48f2590e12562
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-17 00:15:59 +0300

    17/09/23 dated committing

---
+++ src/Java002_Operators/CallingFromClass001/Class2.java
@@ -0,0 +1,8 @@
+package Java002_Operators.CallingFromClass001;
+
+public class Class2 {
+    public static void main(String[] args) {
+        Class1 class1 = new Class1();
+        class1.aKindOfCalc(class1.aNumber);
+    }
+}
