fb7940eb2fda560e3be6889036a50b0bfbc375a3
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-17 00:16:11 +0300

    17/09/23 dated committing

---
+++ src/Java001_Scanner/CallingFromClass009/Class02.java
@@ -0,0 +1,8 @@
+package Java001_Scanner.CallingFromClass009;
+
+public class Class02 {
+    public static void main(String[] args) {
+        Class01 class01 = new Class01();
+        class01.caculatingSleepingTime(class01.sleepingHours);
+    }
+}
