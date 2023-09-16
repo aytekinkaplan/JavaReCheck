e390c117848cdef3784b9cf90c6a6e59cf98a1f1
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-17 00:17:16 +0300

    17/09/23 dated committing

---
+++ src/Java001_Scanner/CallingFromClass010/Cm.java
@@ -0,0 +1,8 @@
+package Java001_Scanner.CallingFromClass010;
+
+public class Cm {
+    public static void main(String[] args) {
+        Cl cl = new Cl();
+        cl.sportCenterPrice(cl.name, cl.age, cl.mass, cl.height, cl.registredTime);
+    }
+}
