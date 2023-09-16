6cf015140eb61d5302c16e2395dce94a11eed2eb
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-17 00:14:46 +0300

    17/09/23 dated committing

---
+++ src/Java002_Operators/ArithMeticExample003.java
@@ -0,0 +1,26 @@
+package Java002_Operators;
+
+public class ArithMeticExample003 {
+    public static void main(String[] args) {
+        System.out.println("Mutlak Değer: "+Math.abs(-45));
+        System.out.println("En büyük sayı: "+Math.max(56,78));
+        System.out.println("En küçük sayı: "+Math.min(56,78));
+        System.out.println("En yakın tam sayıya yuvarlar: "+Math.round(34.6));
+        System.out.println("En yakın tam sayıya yuvarlar: "+Math.round(34.3));
+        System.out.println("Sayının karesini almak: "+Math.pow(Math.pow(2,3),Math.max(3,6)));
+        System.out.println("Sayının karekökünü bulmak: "+Math.sqrt(9));
+        System.out.println("Sayının karekökünü bulmak: "+Math.sqrt(10));
+        System.out.println("Sayının karekökünü bulmak: "+Math.sqrt(10));
+        System.out.println("Sayıyı küçük integera yuvarlar: "+Math.ceil(3.1));
+        System.out.println("Sayıyı büyük integera yuvarlar: "+Math.floor(3.1));
+        System.out.println("Sayının sin değerini verir: "+Math.sin(30));
+        System.out.println("Sayının cos değerini verir: "+Math.cos(30));
+
+        int age = 35;
+        System.out.println(age);
+        System.out.println(age + 3);
+        System.out.println(age);
+        age = age +3;
+        System.out.println(age);
+    }
+}
