5e5d6f8bc86ab3776b540d2d81e89530a9fd19d4
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-17 00:17:42 +0300

    17/09/23 dated committing

---
+++ src/Java002_Operators/ComparisonOperators003.java
@@ -0,0 +1,21 @@
+package Java002_Operators;
+
+public class ComparisonOperators003 {
+
+    public static void main(String[] args) {
+  String str1 = "Techno Study";
+  String str2 = "Techno Study";
+        System.out.println(!(str1 == str2));
+        System.out.println((str1 == str2));
+
+        double double1=12.34;
+        double double2=12.33;
+        System.out.println(double1 > double2);
+        System.out.println(double1 < double2);
+
+        String a = "Kerem Bey";
+        String b = "kerem bey";
+        System.out.println(a == b);
+
+    }
+}
