8950d57475a659b5804b3479672e39150cddb168
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-09-18 02:04:23 +0300

    18/09/23 dated committing

---
+++ src/Java007_MethodCreation/CallingFromClass001/Class02.java
@@ -0,0 +1,11 @@
+package Java007_MethodCreation.CallingFromClass001;
+
+public class Class02 {
+    public static void main(String[] args) {
+        Class01 class01A = new Class01();
+        class01A.myMethod1();
+
+        Class01 class01B = new Class01();
+        class01B.myMethod2();
+    }
+}
