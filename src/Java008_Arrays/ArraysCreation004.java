b8250d5325406782a4b712f503e04e7b3bb409ad
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-10-06 15:06:23 +0300

    06/10/23 dated committing--Murat Abi yi beklerken

---
+++ src/Java008_Arrays/ArraysCreation004.java
@@ -0,0 +1,16 @@
+package Java008_Arrays;
+
+public class ArraysCreation004 {
+    public static void main(String[] args) {
+         /*          int Array oluşturun.
+                elemanları : 13, 15,14,16,16
+                Arrayin elemanlarını yazdırın.
+     */
+        // Kodu aşağıya yazınız..
+
+        int[] dizi = {13,15,14,16,16};
+        for (int i = 0; i < dizi.length; i++) {
+            System.out.print(i+" ");
+        }
+    }
+}
