e1bd90f47fa68acbff60b483a24ccd3ee8994606
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-10-06 15:07:05 +0300

    06/10/23 dated committing--Murat Abi yi beklerken

---
+++ src/Java008_Arrays/ArraysCreation007.java
@@ -0,0 +1,15 @@
+package Java008_Arrays;
+
+public class ArraysCreation007 {
+       /*
+        String array  oluşturun ve elemanları: "new jersey" , "new york", "boston","California"
+        Array'daki eleman sayısını yazdırınız.
+        Cevap 4 olmalı.
+         */
+
+    // Kodu aşağıya yazınız..
+    public static void main(String[] args) {
+        String[] strings = {"new jersey" , "new york", "boston","California"};
+        System.out.println(strings.length);
+    }
+}
