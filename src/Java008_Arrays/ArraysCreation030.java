508bfb2f02053e0360548d3f54b25bf63eb683c8
AYTEKİN KAPLAN <aoniki2018@gmail.com>
2023-10-06 15:10:37 +0300

    06/10/23 dated committing--Murat Abi yi beklerken

---
+++ src/Java008_Arrays/ArraysCreation030.java
@@ -0,0 +1,14 @@
+package Java008_Arrays;
+
+public class ArraysCreation030 {
+    public static void main(String[] args) {
+        String string ="AYTEKİN";
+        System.out.println(string.substring(0, 1) +
+                string.substring(1, 2) +
+                string.substring(2, 3) +
+                string.substring(3, 4) +
+                string.substring(4, 5) +
+                string.substring(5, 6) +
+                string.substring(6, 7));
+    }
+}
